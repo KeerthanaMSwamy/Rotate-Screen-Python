2977e90a574c228cc65ef7d1d9a7aad45a8420e9
Keerthana MS <75252868+KeerthanaMSwamy@users.noreply.github.com>
2023-01-14 22:11:25 +0530

    Updates main.py
    added comments for better understanding

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import rotatescreen
 import time
 screen = rotatescreen.get_primary_display()
-#screen.rotate_to(0)
+#you can change the range to any number u like for the screen to rotate as many times as u want
+#the screen can only be roates by 90 each time and that is why you use i*90
 for i in range (10):
     time.sleep(1)
     screen.rotate_to(i*90 % 360)
